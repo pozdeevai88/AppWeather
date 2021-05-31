7c9cc4bddde65fe4ffbced914703f0615211114f
Aleksey <alexcorpetf@gmail.com>
2021-05-31 17:06:43 +1000

    lesson 5 finished

--- app/src/main/java/ru/geekbrains/appweather/ui/home/DetailsFragment.kt
+++ app/src/main/java/ru/geekbrains/appweather/ui/home/DetailsFragment.kt
@@ -2,35 +2,30 @@ package ru.geekbrains.appweather.ui.home
 
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
-import com.google.gson.Gson
 import ru.geekbrains.appweather.R
 import ru.geekbrains.appweather.Weather
 import ru.geekbrains.appweather.WeatherDTO
 import ru.geekbrains.appweather.databinding.FragmentDetailsBinding
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.MalformedURLException
-import java.net.URL
-import java.util.stream.Collectors
-import javax.net.ssl.HttpsURLConnection
-
-private const val FORECA_API_USER = "<aleksey>"
-private const val FORECA_API_PASSWORD = "HNWSQgufJcV1"
-private const val YANDEX_API_KEY = "eaf23bec-f69d-4c86-843e-d4c544e66f2b"
 
 class DetailsFragment : Fragment() {
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
     private lateinit var weatherBundle: Weather
-
+    private val onLoadListener: WeatherLoader.WeatherLoaderListener =
+        object : WeatherLoader.WeatherLoaderListener {
+            override fun onLoaded(weatherDTO: WeatherDTO) {
+                displayWeather(weatherDTO)
+            }
+            override fun onFailed(throwable: Throwable) {
+                //Обработка ошибки
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,7 +41,8 @@ class DetailsFragment : Fragment() {
         weatherBundle = arguments?.getParcelable(BUNDLE_EXTRA) ?: Weather()
         binding.mainView.visibility = View.GONE
         binding.loadingLayout.visibility = View.VISIBLE
-        loadWeather()
+        val loader = WeatherLoader(onLoadListener, weatherBundle.city.lat, weatherBundle.city.lon)
+        loader.loadWeather()
     }
 
     companion object {
@@ -73,45 +69,5 @@ class DetailsFragment : Fragment() {
             temperatureValue.text = weatherDTO.fact?.temp.toString()
             feelsLikeValue.text = weatherDTO.fact?.feels_like.toString()
         }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun loadWeather() {
-        try {
-//          val uri = URL("https://pfa.foreca.com/api/v1/current/${weatherBundle.city.lon},${weatherBundle.city.lat}")
-            val uri =
-                URL("https://api.weather.yandex.ru/v2/forecast?lat=${weatherBundle.city.lat}&lon=${weatherBundle.city.lon}")
-            val handler = Handler()
-            Thread(Runnable {
-                lateinit var urlConnection: HttpsURLConnection
-                try {
-
-                    urlConnection = uri.openConnection() as HttpsURLConnection
-                    urlConnection.requestMethod = "GET"
-                    urlConnection.addRequestProperty(
-                        "X-Yandex-API-Key",
-                        YANDEX_API_KEY
-                    )
-                    urlConnection.readTimeout = 1000
-                    val bufferedReader = BufferedReader(InputStreamReader(urlConnection.inputStream))
-                    val weatherDTO: WeatherDTO = Gson().fromJson(getLines(bufferedReader), WeatherDTO::class.java)
-                    handler.post { displayWeather(weatherDTO) }
-                } catch (e: Exception) {
-                    Log.e("", "Fail connection", e)
-                    e.printStackTrace()
-                } finally {
-                    urlConnection.disconnect()
-                }
-            }).start()
-        } catch (e: MalformedURLException) {
-            Log.e("", "Fail URI", e)
-            e.printStackTrace()
-
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun getLines(reader: BufferedReader): String {
-        return reader.lines().collect(Collectors.joining("\n"))
     }
 }
