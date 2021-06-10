067af4e5d7db360a0ab7293101bc192c16645a19
Aleksey <alexcorpetf@gmail.com>
2021-06-10 21:16:34 +1000

    Contacts
    Отображаем список контактов

--- app/src/main/java/ru/geekbrains/appweather/ui/contacts/ContactsFragment.kt
+++ app/src/main/java/ru/geekbrains/appweather/ui/contacts/ContactsFragment.kt
@@ -1,22 +1,24 @@
 package ru.geekbrains.appweather.ui.contacts
 
-import android.os.Build
+import android.Manifest
+import android.content.ContentResolver
+import android.content.Context
+import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import ru.geekbrains.appweather.R
 import ru.geekbrains.appweather.databinding.FragmentContactsBinding
-import ru.geekbrains.appweather.databinding.FragmentGalleryBinding
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.MalformedURLException
-import java.net.URL
-import java.util.stream.Collectors
-import javax.net.ssl.HttpsURLConnection
+
+const val REQUEST_CODE = 42
 
 class ContactsFragment : Fragment() {
 
@@ -34,13 +36,116 @@ class ContactsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        checkPermission()
 //        binding.#####.setOnClickListener(clickListener)
+    }
+
+    private fun checkPermission() {
+        context?.let {
+            when {
+                ContextCompat.checkSelfPermission(it,Manifest.permission.READ_CONTACTS) ==
+                        PackageManager.PERMISSION_GRANTED -> {
+                        //Доступ к контактам на телефоне есть
+                        getContacts()
+                        }
+                //Опционально: если нужно пояснение перед запросом разрешений
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> {
+                    AlertDialog.Builder(it)
+                        .setTitle("Доступ к контактам")
+                        .setMessage("Объяснение")
+                        .setPositiveButton("Предоставить доступ") { _, _ ->
+                            requestPermission()
+                        }
+                        .setNegativeButton("Не надо") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        .create()
+                        .show()
+                }
+                else -> {
+                    //Запрашиваем разрешение
+                    requestPermission()
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+                REQUEST_CODE -> {
+                // Проверяем, дано ли пользователем разрешение по нашему запросу
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    getContacts()
+                } else {
+                    // Поясните пользователю, что экран останется пустым, потому что доступ к контактам не предоставлен
+                    context?.let {
+                        AlertDialog.Builder(it)
+                            .setTitle("Доступ к контактам")
+                            .setMessage("Объяснение")
+                            .setNegativeButton("Закрыть") { dialog, _ ->
+                                dialog.dismiss() }
+                            .create()
+                            .show()
+                    }
+                }
+                    return
+                }
+        }
+    }
+
+    private fun getContacts() {
+        context?.let {
+            // Получаем ContentResolver у контекста
+            val contentResolver: ContentResolver = it.contentResolver
+            // Отправляем запрос на получение контактов и получаем ответ в виде Cursor'а
+            val cursorWithContacts: Cursor? = contentResolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                null,
+                null,
+                null,
+                ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+            )
+            cursorWithContacts?.let { cursor ->
+                for (i in 0..cursor.count) {
+                    // Переходим на позицию в Cursor'е
+                    if (cursor.moveToPosition(i)) {
+                        // Берём из Cursor'а столбец с именем
+                        val name =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                        addView(it, name)
+                    }
+                }
+            }
+            cursorWithContacts?.close()
+        }
+    }
+
+    private fun addView(context: Context, textToShow: String) {
+        binding.containerForContacts.addView(AppCompatTextView(context).apply {
+            text = textToShow
+            textSize = resources.getDimension(R.dimen.main_container_text_size)
+            setOnClickListener {
+                
+            }
+        })
+    }
+
+    private fun requestPermission() {
+        requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
     }
 
     private var clickListener: View.OnClickListener = object : View.OnClickListener {
         override fun onClick(v: View?) {
 
         }
+    }
 
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            ContactsFragment()
     }
 }
