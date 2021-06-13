66a6cd3ee4bf679a77e69e0e423684a50ae58308
Aleksey <alexcorpetf@gmail.com>
2021-06-13 21:49:21 +1000

    Геолокация и погода
    Определяем погоду по координатам устройства

--- app/src/main/java/ru/geekbrains/appweather/ui/home/HomeFragment.kt
+++ app/src/main/java/ru/geekbrains/appweather/ui/home/HomeFragment.kt
@@ -1,20 +1,34 @@
 package ru.geekbrains.appweather.ui.home
 
+import android.Manifest
+import android.app.AlertDialog
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_home.*
 import ru.geekbrains.appweather.R
 import ru.geekbrains.appweather.databinding.FragmentHomeBinding
+import ru.geekbrains.appweather.model.City
 import ru.geekbrains.appweather.model.Weather
 import ru.geekbrains.appweather.viewmodel.AppState
 import ru.geekbrains.appweather.viewmodel.HomeViewModel
+import java.io.IOException
 
 private const val IS_WORLD_KEY = "LIST_OF_TOWNS_KEY"
+private const val LOCATION_REQUEST_CODE = 44
+private const val REFRESH_PERIOD = 60000L
+private const val MINIMAL_DISTANCE = 100f
 
 class HomeFragment : Fragment() {
 
@@ -24,16 +38,26 @@ class HomeFragment : Fragment() {
 
     private val adapter = HomeFragmentAdapter(object : OnItemViewClickListener {
         override fun onItemViewClick(weather: Weather) {
-            activity?.supportFragmentManager?.apply {
-                beginTransaction()
-                    .replace(R.id.nav_host_fragment, DetailsFragment.newInstance(Bundle().apply {
-                        putParcelable(DetailsFragment.BUNDLE_EXTRA, weather)
-                    }))
-                    .addToBackStack("")
-                    .commitAllowingStateLoss()
-            }
+            openDetailsFragment(weather)
+//            activity?.supportFragmentManager?.apply {
+//                beginTransaction()
+//                    .replace(R.id.nav_host_fragment, DetailsFragment.newInstance(Bundle().apply {
+//                        putParcelable(DetailsFragment.BUNDLE_EXTRA, weather)
+//                    }))
+//                    .addToBackStack("")
+//                    .commitAllowingStateLoss()
+//            }
         }
     })
+
+    private val onLocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            context?.let { getAddressAsync(it, location) }
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
 
 //    private val broadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
 //        override fun onReceive(context: Context?, intent: Intent?) {
@@ -66,10 +90,200 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.mainFragmentRecyclerView.adapter = adapter
         binding.mainFragmentFAB.setOnClickListener { changeWeatherDataSet() }
+        binding.mainFragmentFABLocation.setOnClickListener { checkPermission() }
         homeViewModel.getLiveData().observe(viewLifecycleOwner, { renderData(it) })
         showListOfTowns()
 //        val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
 //        context?.registerReceiver(broadcastReceiver, filter)
+    }
+
+    private fun checkPermission() {
+        activity?.let {
+            when {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    getLocation()
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                    showRationaleDialog()
+                }
+                else -> {
+                    requestPermission()
+                }
+            }
+        }
+    }
+
+    private fun showRationaleDialog() {
+        activity?.let {
+            AlertDialog.Builder(it)
+                .setTitle(getString(R.string.dialog_rationale_title))
+                .setMessage(getString(R.string.dialog_rationale_meaasge))
+                .setPositiveButton(getString(R.string.dialog_rationale_give_access))
+                { _, _ -> requestPermission() }
+                .setNegativeButton(getString(R.string.dialog_rationale_decline)) { dialog, _ -> dialog.dismiss() }
+                .create()
+                .show()
+        }
+    }
+
+    private fun requestPermission() {
+        requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        checkPermissionsResult(requestCode, grantResults)
+    }
+
+    private fun checkPermissionsResult(requestCode: Int, grantResults: IntArray) {
+        when (requestCode) {
+            LOCATION_REQUEST_CODE -> {
+                var grantedPermissions = 0
+                if ((grantResults.isNotEmpty())) {
+                    for (i in grantResults) {
+                        if (i == PackageManager.PERMISSION_GRANTED) {
+                            grantedPermissions++
+                        }
+                    }
+                    if (grantResults.size == grantedPermissions) {
+                        getLocation()
+                    } else {
+                        showDialog(
+                            getString(R.string.dialog_title_no_gps),
+                            getString(R.string.dialog_message_no_gps)
+                        )
+                    }
+                } else {
+                    showDialog(
+                        getString(R.string.dialog_title_no_gps),
+                        getString(R.string.dialog_message_no_gps)
+                    )
+                }
+                return
+            }
+        }
+    }
+
+    private fun showDialog(title: String, message: String) {
+        activity?.let {
+            AlertDialog.Builder(it)
+                .setTitle(title)
+                .setMessage(message)
+                .setNegativeButton(getString(R.string.dialog_button_close)) { dialog,
+                                                                              _ ->
+                    dialog.dismiss()
+                }
+                .create()
+                .show()
+        }
+    }
+
+    private fun getLocation() {
+        activity?.let { context ->
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+
+                // Получить менеджер геолокаций
+                val locationManager =
+                    context.getSystemService(Context.LOCATION_SERVICE) as
+                            LocationManager
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    val provider =
+                        locationManager.getProvider(LocationManager.GPS_PROVIDER)
+                    provider?.let {
+                        // Будем получать геоположение через каждые 60 секунд или каждые 100 метров
+                        locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            REFRESH_PERIOD,
+                            MINIMAL_DISTANCE,
+                            onLocationListener
+                        )
+                    }
+                } else {
+                    val location =
+                        locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    if (location == null) {
+                        showDialog(
+                            getString(R.string.dialog_title_gps_turned_off),
+                            getString(R.string.dialog_message_last_location_unknown)
+                        )
+                    } else {
+                        getAddressAsync(context, location)
+                        showDialog(
+                            getString(R.string.dialog_title_gps_turned_off),
+                            getString(R.string.dialog_message_last_known_location)
+                        )
+                    }
+                }
+            } else {
+                showRationaleDialog()
+            }
+        }
+    }
+
+    private fun getAddressAsync(
+        context: Context,
+        location: Location
+    ) {
+        val geoCoder = Geocoder(context)
+        Thread {
+            try {
+                val addresses = geoCoder.getFromLocation(
+                    location.latitude,
+                    location.longitude,
+                    1
+                )
+                mainFragmentFAB.post { showAddressDialog(addresses[0].getAddressLine(0), location) }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
+
+    private fun showAddressDialog(address: String, location: Location) {
+        activity?.let {
+            AlertDialog.Builder(it)
+                .setTitle(getString(R.string.dialog_address_title))
+                .setMessage(address)
+                .setPositiveButton(getString(R.string.dialog_address_get_weather)) {
+                        _, _ ->
+                    openDetailsFragment(
+                        Weather(
+                            City(
+                                address,
+                                location.latitude,
+                                location.longitude
+                            )
+                        )
+                    )
+                }
+                .setNegativeButton(getString(R.string.dialog_button_close)) { dialog,
+                                                                              _ -> dialog.dismiss() }
+                .create()
+                .show()
+        }
+    }
+
+    private fun openDetailsFragment(
+        weather: Weather
+    ) {
+        activity?.supportFragmentManager?.apply {
+            beginTransaction()
+                .replace(R.id.nav_host_fragment, DetailsFragment.newInstance(Bundle().apply {
+                        putParcelable(DetailsFragment.BUNDLE_EXTRA, weather)
+                    }))
+                .addToBackStack("")
+                .commitAllowingStateLoss()
+        }
     }
 
     private fun changeWeatherDataSet() {
